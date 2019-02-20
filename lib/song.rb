c31647ccf29fb4582c928f35bb9fb7babe6d5d0a
Alicia Santiago <albrown07@mac.com>
2019-02-20 16:40:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,8 +26,15 @@ class Song
     
     def self.genre_count
       genre_count = {}
-      
-    end 
+        @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
     
   @@artists = []
   
