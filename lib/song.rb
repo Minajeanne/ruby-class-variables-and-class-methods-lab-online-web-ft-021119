96cf5a6f1c3abee0e6bf1e107021161b877a3fe2
Alicia Santiago <albrown07@mac.com>
2019-02-20 15:57:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,19 @@
 require 'pry'
 
 class Song 
+  attr_accessor :song_name, :artists, :genre 
   
-  @@song_count = 0 
+  @@count = 0 
+  
+   def initialize
+    @@count += 1
+  end
+ 
+  def self.count
+    @@count
+  end
+  
+  
   
   
 end
